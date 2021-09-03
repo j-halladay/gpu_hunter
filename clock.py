1edc9c47c81a62fe541a342c388cb1436344e265
Jon <golee10000@hotmail.com>
2021-09-03 13:57:12 -0400

    fixing schedule

--- clock.py
+++ clock.py
@@ -1,8 +1,8 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+import main 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=45)
+@sched.main('interval', minutes=45)
 def timed_job():
     print('This job is run every forty five minutes.')
 
