7279405eec858b0518b6f4cc3434ab8d32a6e56e
Jon <golee10000@hotmail.com>
2021-09-03 13:46:17 -0400

    fixed indent

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ if __name__ == '__main__':
     # while x != 1:
 
 
-        scraped_data = scraper.scraper()
-        if scraped_data != False:
-            print('Found something! Sending Report!')
-            
-            if scraped_data['error'] == True:
-                if error_message_sent == False:
-                    error_message_sent = True
-            print(str(scraped_data['message']))
-            scraper.send_sms(str(scraped_data["message"]))
-        else:
-            print('Nothing found!')
-            # uncomment scraper.send_sms() for production
+    scraped_data = scraper.scraper()
+    if scraped_data != False:
+        print('Found something! Sending Report!')
+        
+        if scraped_data['error'] == True:
+            if error_message_sent == False:
+                error_message_sent = True
+        print(str(scraped_data['message']))
+        scraper.send_sms(str(scraped_data["message"]))
+    else:
+        print('Nothing found!')
+        # uncomment scraper.send_sms() for production
 
            
         # time.sleep(1200)
